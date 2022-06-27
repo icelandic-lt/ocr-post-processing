cb686d3307c1d56264e6fa40e0d7e09f999b6135
atlijas <steinunn@pop-os.localdomain>
2022-06-27 17:22:51 +0000

    pair lines

--- utils/pair_lines.py
+++ utils/pair_lines.py
@@ -27,6 +27,32 @@ def n_good_words(line):
     sensemaking_words = [word for word in line.split() if makes_sense(clean_token(word))]
     return (len(sensemaking_words))
 
+def fix_guillemets(line):
+    return line.replace('» ', ' »').replace(' «', '« ').strip()
+
+
+def get_best_first_half_of_compound_line(line1, line2, line3):
+    line_1_1 = line1.split('<newline>')[0].strip()
+    line_1_2 = line1.split('<newline>')[1].strip()
+    line_2_1 = line2.split('<newline>')[0].strip()
+    line_2_2 = line2.split('<newline>')[1].strip()
+    line_3_1 = line3.split('<newline>')[0].strip()
+    line_3_2 = line3.split('<newline>')[1].strip()
+
+    last_token_in_line_1_2 = {'1_2': clean_token(line_1_2.split()[-1])}
+    last_token_in_line_2_1 = {'2_1': clean_token(line_2_1.split()[-1])}
+    first_token_in_line_2_2 = {'2_2': clean_token(line_2_2.split()[0])}
+    first_token_in_line_3_1 = {'3_1': clean_token(line_3_1.split()[0])}
+
+    cand_1 = [line_1_2, last_token_in_line_1_2.get('1_2') + first_token_in_line_2_2.get('2_2')]
+    cand_2 = [line_1_2, last_token_in_line_1_2.get('1_2') + first_token_in_line_3_1.get('3_1')]
+    cand_3 = [line_2_1, last_token_in_line_2_1.get('2_1') + first_token_in_line_2_2.get('2_2')]
+    cand_4 = [line_2_1, last_token_in_line_2_1.get('2_1') + first_token_in_line_3_1.get('3_1')]
+
+    good_first_half = [fh[0] for fh in [cand_1, cand_2, cand_3, cand_4] if makes_sense(fh[1])][0]
+    return good_first_half
+
+
 def process_lines(lines):
             """
             Iterate over all the lines in a file. The format is as such:
@@ -56,6 +82,7 @@ def process_lines(lines):
 
                 if line_is_split:
                     curr_secnd_half = lines[current_index].split('<newline>')[1].strip()
+                    # current_line_out set as curr_secnd_half.
                     current_line_out = curr_secnd_half
 
                 if not curr_line_is_last_in_file:
@@ -64,24 +91,77 @@ def process_lines(lines):
                     if next_line_is_split:
                         next_secnd_half = lines[current_index+1].split('<newline>')[1]
 
+                # This is only for the fist line. Special case because of my indexing. Not ideal, I know.
                 if current_index == 0:
                     yield curr_first_half
 
                 else:
-                    if curr_secnd_half and next_first_half:
-                        n_good_curr = n_good_words(curr_secnd_half)
-                        n_good_next = n_good_words(next_first_half)
-                        if n_good_curr >= n_good_next:
-                            current_line_out = curr_secnd_half
+                    # The two candidates will most of the time be the same.
+                    if curr_secnd_half == next_first_half:
+                        current_line_out = curr_secnd_half
+                    else:
+                        if curr_secnd_half.endswith('-'):
+                            current_line_out = get_best_first_half_of_compound_line(lines[current_index],
+                                                                                    lines[current_index+1],
+                                                                                    lines[current_index+2])
                         else:
-                            current_line_out = next_first_half
-                    # if curr_secnd_half == next_first_half:
-                    #     current_line_out = curr_secnd_half
-                    # else:
-                    #     current_line_out = next_first_half
+                            n_good_curr = n_good_words(curr_secnd_half)
+                            n_good_next = n_good_words(next_first_half)
+                            if n_good_curr > n_good_next:
+                                # print(curr_secnd_half.split(), next_first_half.split())
+                                current_line_out = curr_secnd_half
+                            else:
+                                current_line_out = next_first_half
+
+
                 if current_line_out is not None and not ((current_index + 1) == n_lines and current_line_out == ''):
                     yield current_line_out
                 current_index += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    # If they're not the same:
+                    # else:
+                        # if curr_secnd_half:
+                        #     if curr_secnd_half.endswith('-'):
+                                # Remove trailing hyphen from the last word in current line
+                                # curr_secnd_half_last_token = curr_secnd_half.split()[-1].rstrip('-')
+                                # first_word_in_curr_secnd_half = ''
+                                # first_word_in_next_first_half = ''
+                    #
+                    #         try:
+                    #             first_word_in_curr_secnd_half = curr_secnd_half.split()[0]
+                    #         except IndexError:
+                    #             pass
+                    #         try:
+                    #             first_word_in_next_first_half = next_first_half.split()[0]
+                    #         except IndexError:
+                    #             pass
+                    #
+                    #         curr_hyphen_cand = last_word_in_curr_first_half + first_word_in_curr_secnd_half
+                    #         next_hyphen_cand = last_word_in_curr_first_half + first_word_in_next_first_half
+                    #
+                    #         curr_hyphen_cand_good = makes_sense(curr_hyphen_cand.strip(punctuation))
+                    #         next_hyphen_cand_good = makes_sense(next_hyphen_cand.strip(punctuation))
+
+
+
 
 
 
