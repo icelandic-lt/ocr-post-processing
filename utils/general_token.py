21892676f1dbaf39534dde804dc3a35ed8069dad
atlijas <steinunn@pop-os.localdomain>
2022-07-04 17:40:14 +0000

    work

--- utils/general_token.py
+++ utils/general_token.py
@@ -19,6 +19,9 @@ def gen_non_overlapping_ngrams(chars, ngr):
 def count_chars(token):
     return len([char for char in token if char.isalpha()])
 
+def exists_in_bin(token):
+    return bin_conn.lookup(token) != []
+
 class TextToken:
     def __init__(self, token):
         # The token exactly as it appears in the line. Tokens are gotten by
@@ -53,6 +56,113 @@ class TextToken:
 
     def __str__(self):
         return self.original_token
+
+
+class OCRToken:
+    """
+    This class represents a single token, which will be read from a single line
+    at a time in other modules. Its attributes mostly have to do with various
+    versions of the token, i.e. with and without punctuation, lower and
+    titleized versions of it, etc. Some other attributes are its "Icelandicness",
+    i.e. how frequently its character trigrams appear in a specific corpus,
+    and mutated versions of it which are made by performing character substitutions
+    based on how frequently said substitutions appear in an original/corrected
+    OCR parallel corpus.
+    """
+    def __init__(self, token):
+        # The token exactly as it appears in the line. Tokens are gotten by
+        # splitting a line on spaces (\s)
+        self.original_token = token
+        self.is_punct = all(char in punctuation for char in self.original_token)
+        # A list of punctuation marks (excl. hyphens) and character strings
+        # (a word can contain a hyphen) of a given OCRed token.
+        self.token_and_punct = re.findall(r'[\w-]+|[^\s\w]', self.original_token)
+
+        # The longest character string (possibly including a hyphen)
+        # of self.token_and_punct
+        self.clean = max(self.token_and_punct, key=len)
+
+        # Boolean. Whether self.clean consists of only numeric characters
+        self.is_number = all(char.isnumeric() or char in [',', '.', '-'] for char in (str(self.original_token)))
+
+        # Boolean. Whether a token contains any numeric characters, but is not wholly numeric.
+        self.contains_number = any(char.isnumeric() for char in self.clean) and not self.is_number
+
+        # Boolean. Whether a token contains punctuation (other than a hyphen)
+        # but doesn't start or end with it.
+        self.contains_bad_punct = any(char in punctuation for char in self.original_token
+                                  if char != '-' and not token.startswith(char)
+                                  and not token.endswith(char)
+                                  and char != '-')
+
+        self.startswith_punct = self.original_token[0] in punctuation
+        self.endswith_punct = (self.original_token[-1] in punctuation
+                               and not self.original_token[-1] == '-')
+
+        self.last_character_index = max([index for index, char in enumerate(self.original_token)
+                                        if char not in punctuation],
+                                        default=len(self.original_token))
+
+        self.first_character_index = min([index for index, char in enumerate(self.original_token)
+                                         if char not in punctuation],
+                                         default=len(self.original_token))
+
+        self.start_punct_index = [index for index, char in enumerate(self.original_token)
+                                  if char in punctuation and index < self.first_character_index]
+
+        self.end_punct_index = [index for index, char in enumerate(self.original_token)
+                                if char in punctuation and index > self.last_character_index]
+
+    
+
+    def _check_if_possibly_compound(self):
+        """
+        This function can be used to check whether certain words "make sense"
+        as an Icelandic compound word.
+        """
+        bin_lookup = Bin()
+        return bin_lookup.lookup(self.original_token)[1] != []
+
+
+    def mutate_token_with_unigram_replacements(self):
+        """
+        This function mutates a token based on known character substitutions
+        (see: get_char_replacements) and returns all the possibilities if they
+        exist in a dictionary.
+        """
+        all_possible_replacements = []
+        length_of_product = reduce(mul, map(len, self.char_unigrams), 1)
+        if length_of_product > 1000:
+            return []
+        for unigram in self.char_unigrams:
+            possible_replacements = [char for char, freq in
+                                     get_char_replacements(unigram[1], freq=3)]
+            all_possible_replacements.append([unigram[1]] + possible_replacements[0:2])
+        all_possible_replacements = [''.join(w) for w in product(*all_possible_replacements)]
+        return [w for w in all_possible_replacements if word_in_any_dict(''.join(w))]
+
+    def mutate_token_with_bigram_replacements(self):
+        """
+        Doesn't work. Could be useful, though.
+        Same as above but replaces possibly erroneous bigrams with known substitutions.
+        """
+        all_possible_replacements = []
+        for bigram in self.char_bigrams:
+            possible_replacements = [char for char, freq in
+                                     get_char_replacements(bigram[1], freq=3)]
+            all_possible_replacements.append([bigram[1]] + possible_replacements[0:2])
+        print(all_possible_replacements)
+        all_possible_replacements = [''.join(w) for w in product(*all_possible_replacements)]
+        return [w for w in all_possible_replacements if word_in_any_dict(''.join(w))]
+
+    def __str__(self):
+        return self.original_token
+
+    def __repr__(self):
+        return self.original_token
+
+    def __len__(self):
+        return len(self.original_token)
 
 if __name__ == '__main__':
     pass
--- utils/pair_lines.py
+++ utils/pair_lines.py
@@ -1,10 +1,12 @@
 
+from genericpath import exists
 from islenska.bincompress import BinCompressed
 from islenska import Bin
 from string import punctuation
 import pickle
 from Levenshtein import distance
 from substitution_token import get_similar_by_known_subs
+from general_token import TextToken, OCRToken
 from itertools import chain
 import re
 from argparse import ArgumentParser
@@ -13,7 +15,8 @@ parser = ArgumentParser()
 parser.add_argument('-f', '--file')
 args = parser.parse_args()
 
-
+with open('../bin_tree.pickle', 'rb') as bt:
+    tree = pickle.load(bt)
 
 
 punctuation += "–„”—«»"
@@ -40,11 +43,6 @@ def makes_sense(token):
 def exists_in_bin(token):
     return bin_conn.lookup(token) != []
 
-def format_newline_line(line):
-    first_half = line.split('<newline>')[0].strip()
-    second_half = line.split('<newline>')[1].strip()
-    return f'{first_half}\n{second_half}'
-
 
 def read_lines(file):
     with open(file, 'r', encoding='utf-8') as infile:
@@ -57,8 +55,6 @@ def n_good_words(line):
     sensemaking_words = [word for word in line.split() if makes_sense(clean_token(word))]
     return (len(sensemaking_words))
 
-def fix_guillemets(line):
-    return line.replace('» ', ' »').replace(' «', '« ').strip()
 
 def format_line_out(line, junk_list):
     line_out = line
@@ -68,12 +64,8 @@ def format_line_out(line, junk_list):
     return line_out.strip()
 
 
-def get_clean_tokens(lines):
-    junk = ocr_junk + ['-<newline> ', '<newline']
-    for line in lines:
-        line = format_line_out(line, junk_list=junk)
-        print(line)
-
+def any_exists_in_bin(token):
+    return exists_in_bin(token) or exists_in_bin(token.lower()) or exists_in_bin(token.title())
 
 def get_best_first_half_of_compound_line(line1, line2, line3):
     try:
@@ -103,81 +95,112 @@ def get_best_first_half_of_compound_line(line1, line2, line3):
                 cands = compound_makes_sense[0]
         return cands
     except IndexError:
-        return line1
+        return line_1_2
 
 
 def get_best_second_half_of_compound_line(line1, line2, line3):
-    line_1_1 = line1.split('<newline>')[0].strip()
-    line_minus_1_1 = line2.split('<newline>')[0].strip()
-    line_minus_1_2 = line2.split('<newline>')[1].strip()
-    line_minus_2_2 = line3.split('<newline>')[1].strip()
+    try:
+        line_1_1 = line1.split('<newline>')[0].strip()
+        line_minus_1_1 = line2.split('<newline>')[0].strip()
+        line_minus_1_2 = line2.split('<newline>')[1].strip()
+        line_minus_2_2 = line3.split('<newline>')[1].strip()
 
-    first_token_in_line_1_1 = {'1_1': clean_token(line_1_1.split()[0])}
-    first_token_in_line_minus_1_2 = {'minus_1_2': clean_token(line_minus_1_2.split()[0])}
-    last_token_in_line_minus_1_1 = {'minus_1_1': clean_token(line_minus_1_1.split()[-1])}
-    last_token_in_line_minus_2_2 = {'minus_2_2': clean_token(line_minus_2_2.split()[-1])}
+        first_token_in_line_1_1 = {'1_1': clean_token(line_1_1.split()[0])}
+        first_token_in_line_minus_1_2 = {'minus_1_2': clean_token(line_minus_1_2.split()[0])}
+        last_token_in_line_minus_1_1 = {'minus_1_1': clean_token(line_minus_1_1.split()[-1])}
+        last_token_in_line_minus_2_2 = {'minus_2_2': clean_token(line_minus_2_2.split()[-1])}
 
-    cand_1 = [line_1_1, last_token_in_line_minus_1_1.get('minus_1_1') + first_token_in_line_1_1.get('1_1')]
-    cand_2 = [line_1_1, last_token_in_line_minus_2_2.get('minus_2_2') + first_token_in_line_1_1.get('1_1')]
-    cand_3 = [line_minus_1_2, last_token_in_line_minus_1_1.get('minus_1_1') + first_token_in_line_minus_1_2.get('minus_1_2')]
-    cand_4 = [line_minus_1_2, last_token_in_line_minus_2_2.get('minus_2_2') + first_token_in_line_minus_1_2.get('minus_1_2')]
+        cand_1 = [line_1_1, last_token_in_line_minus_1_1.get('minus_1_1') + first_token_in_line_1_1.get('1_1')]
+        cand_2 = [line_1_1, last_token_in_line_minus_2_2.get('minus_2_2') + first_token_in_line_1_1.get('1_1')]
+        cand_3 = [line_minus_1_2, last_token_in_line_minus_1_1.get('minus_1_1') + first_token_in_line_minus_1_2.get('minus_1_2')]
+        cand_4 = [line_minus_1_2, last_token_in_line_minus_2_2.get('minus_2_2') + first_token_in_line_minus_1_2.get('minus_1_2')]
 
-    cands = None
+        cands = None
 
-    compound_in_bin = [sh[0] for sh in [cand_1, cand_2, cand_3, cand_4] if exists_in_bin(sh[1])]
+        compound_in_bin = [sh[0] for sh in [cand_1, cand_2, cand_3, cand_4] if exists_in_bin(sh[1])]
 
-    if compound_in_bin:
-        cands = compound_in_bin[0]
-    else:
-        compound_makes_sense = [sh[0] for sh in [cand_1, cand_2, cand_3, cand_4] if makes_sense(sh[1])]
-        if compound_makes_sense:
-            cands = compound_makes_sense[0]
-    return cands
+        if compound_in_bin:
+            cands = compound_in_bin[0]
+        else:
+            compound_makes_sense = [sh[0] for sh in [cand_1, cand_2, cand_3, cand_4] if makes_sense(sh[1])]
+            if compound_makes_sense:
+                cands = compound_makes_sense[0]
+        return cands
+    except IndexError:
+        return line_1_1
 
 def lookup_similar(token):
     lev_dist = 3 if len(token) > 12 else 1
     similar_cands = [tok for dist, tok in tree.find(token, n=lev_dist)]
+    if len(similar_cands) == 0:
+        similar_cands = [tok for dist, tok in tree.find(token.lower(), n=lev_dist)]
     if len(similar_cands) == 1:
         return similar_cands[0]
     else:
-        if similar_cands:
+        if similar_cands:      
             most_similar = [token for token, dist in get_most_similar_from_list(token, similar_cands)]
             known_subs = get_similar_by_known_subs(token, most_similar)
-            print(known_subs)
             if len(known_subs) > 1:
+                filtered_subs = min(get_most_similar_from_list(token, known_subs), key=lambda x: x[1])
+                if filtered_subs:
+                    return filtered_subs[0]
                 return None
             else:
-                return known_subs
+                if known_subs:
+                    return known_subs[0]
+                else:
+                    return None
         else:
             return None
 
-def check_token(token, part_of_compound=False):
-    if part_of_compound:
-        return True
 
-    token = clean_token(token)
-    if exists_in_bin(token):
-        return True
-    if makes_sense(token):
-        return True
-    else:
-        return lookup_similar(token)
-
-# TODO: BÚA TIL LISTA SEM HELDUR UTAN UM ALLAR ORÐMYNDIR Í TEXTANUM
-# ÞVÍ ÞAÐ ER MJÖG LÍKLEGT AÐ SAMA ORÐIÐ KOMI OFTAR FYRIR Í HONUM EN
-# EINU SINNI
+def format_token_out(token, start_punct, end_punct):
+    tok_and_punct = ''
+    if start_punct:
+        tok_and_punct += start_punct
+    tok_and_punct += token
+    if end_punct:
+        tok_and_punct += end_punct
+    return tok_and_punct + ' '
 
 
-# with open('../bin_tree.pickle', 'rb') as bt:
-#     tree = pickle.load(bt)
+def editable(token, index):
+    return (len(token) > 3
+            and not (index != 0 and len(token) < 4)
+            and not token.endswith('-') 
+            and not token.endswith('.'))
 
-# x = ['maður', 'kona', 'hudnur', 'kosninpúrslifin']
 
-# for token in x:
-#     #print(token)
-#     print(check_token(token))
 
-# quit()
+def sub_tokens_in_line(line):
+    line_out = ''
+    for (index, token) in enumerate(line.split()):
+        token_out = token
+        start_punct = None
+        end_punct = None
+        ocr_token = OCRToken(token)
+        if not ocr_token.is_punct:
+            if ocr_token.startswith_punct:
+                start_punct = ocr_token.original_token[ocr_token.start_punct_index[0]:len(ocr_token.start_punct_index)]
+            if ocr_token.endswith_punct:
+                end_punct = ocr_token.original_token[ocr_token.end_punct_index[0]:len(ocr_token)]
+        if any_exists_in_bin(ocr_token.clean) or not editable(token, index):
+            token_out = ocr_token.clean
+        else:
+            similar_token = lookup_similar(ocr_token.clean)
+            if similar_token:
+                token_out = str(similar_token)
+            else:
+                token_out = ocr_token.clean
+        if token.islower():
+            token_out = token_out.lower()
+        elif token.isupper():
+            token_out = token_out.upper()
+        elif token.istitle():
+            token_out = token_out.title()
+        line_out += format_token_out(token_out, start_punct=start_punct, end_punct=end_punct)
+    return line_out.strip()
+
 
 def process_lines(lines):
             """
@@ -198,6 +221,8 @@ def process_lines(lines):
 
                 line_is_split = len(lines[current_index].split('<newline>')) > 1
                 curr_first_half = lines[current_index].split('<newline>')[0].strip()
+
+
 
                 curr_secnd_half = None
                 next_first_half = None
@@ -231,7 +256,6 @@ def process_lines(lines):
                     last_last_secnd_half = last_last_line.split('<newline>')[1].strip()
                 except IndexError:
                     pass
-
                 # This is only for the fist line. Special case because of the indexing. Not ideal, I know.
                 if current_index == 0:
                     yield curr_first_half
@@ -240,14 +264,14 @@ def process_lines(lines):
                     # The two candidates will most of the time be the same.
                     if curr_secnd_half == next_first_half:
                         current_line_out = curr_secnd_half
-                    # else:
-                    #     if curr_secnd_half:
-                    #         if curr_secnd_half.endswith('-'):
-                    #             best_comp_first_half = get_best_first_half_of_compound_line(lines[current_index],
-                    #                                                                         lines[current_index+1],
-                    #                                                                         lines[current_index+2])
-                    #             if best_comp_first_half:
-                    #                 current_line_out = best_comp_first_half
+                    else:
+                        if curr_secnd_half:
+                            if curr_secnd_half.endswith('-'):
+                                best_comp_first_half = get_best_first_half_of_compound_line(lines[current_index],
+                                                                                            lines[current_index+1],
+                                                                                            lines[current_index+2])
+                                if best_comp_first_half:
+                                    current_line_out = best_comp_first_half
                             # else:
                             #     n_good_curr = n_good_words(curr_secnd_half)
                             #     n_good_next = n_good_words(next_first_half)
@@ -265,8 +289,9 @@ def process_lines(lines):
                         # else:
                         #     current_line_out = next_first_half
 
-
+                
                 if current_line_out is not None and not ((current_index + 1) == n_lines and current_line_out == ''):
+                    
                     yield format_line_out(current_line_out, ocr_junk)
                 current_index += 1
 
@@ -274,9 +299,9 @@ def process_lines(lines):
 if __name__ == '__main__':
     out_dir = '../test_data/outputs/'
     test_file = args.file
-    #get_clean_tokens(read_lines(test_file))
-    #test_file = f'{out_dir}less_errors_line_pairs_937152_256_2_1024_16_4_4_0dot1_40_0_3e-05_3000_3_EPOCH_22_althydubladid_1949-2-1-bls-4.txt'
-    #all_tokens = [clean_token(token) for token in (list(chain(*[tok for tok in [line.split(' ') for line in read_lines(test_file)]])))]
     for line in process_lines(read_lines(test_file)):
         print(line)
-        pass
+        # edited = sub_tokens_in_line(line)
+        # if edited:
+        #     print(edited)
+        # pass
