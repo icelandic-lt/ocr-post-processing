1a43ca63f2fa6ec521b676ac18f65f0adba20873
atlijas <steinunn@pop-os.localdomain>
2022-07-14 15:08:17 +0000

    modern test file

--- utils/pair_lines.py
+++ utils/pair_lines.py
@@ -95,7 +95,7 @@ def read_lines(file):
 #         return line_1_1
 
 
-def process_lines(original_lines, corrected_lines):
+def process_lines(original_lines, transformed_lines):
             """
             Iterate over all the lines in a file. The format is as such:
             First part of a sentence <newline> which continues here
@@ -106,74 +106,64 @@ def process_lines(original_lines, corrected_lines):
             transformer's attention mechanism only works on running sequences.
             This helps it learn between lines.
             """
-            n_lines = len(corrected_lines)
+            n_lines = len(transformed_lines)
             current_index = 0
             while current_index < n_lines:
                 current_line_out = ''
                 curr_line_is_last_in_file = current_index+1 == n_lines
 
-                line_is_split = len(corrected_lines[current_index].split('<newline>')) > 1
-                curr_first_half = corrected_lines[current_index].split('<newline>')[0].strip()
+                line_is_split = len(transformed_lines[current_index].split('<newline>')) > 1
+                curr_first_half = transformed_lines[current_index].split('<newline>')[0].strip()
 
 
                 curr_secnd_half = None
                 next_first_half = None
                 
                 next_secnd_half = None
-                last_first_half = None
+                # last_first_half = None
                 last_secnd_half = None
-                last_last_first_half = None
-                last_last_secnd_half = None
-                next_next_first_half = None
-                next_next_secnd_half = None
+                # last_last_first_half = None
+                # last_last_secnd_half = None
+                # next_next_first_half = None
+                # next_next_secnd_half = None
 
                 if line_is_split:
-                    curr_secnd_half = corrected_lines[current_index].split('<newline>')[1].strip()
+                    curr_secnd_half = transformed_lines[current_index].split('<newline>')[1].strip()
                     # current_line_out set as curr_secnd_half.
                     current_line_out = curr_secnd_half
                 else:
                     current_Line_out = curr_first_half
                 if not curr_line_is_last_in_file:
-                    next_first_half = corrected_lines[current_index+1].split('<newline>')[0].strip()
-                    next_line_is_split = len(corrected_lines[current_index+1].split('<newline>')) > 1
+                    next_first_half = transformed_lines[current_index+1].split('<newline>')[0].strip()
+                    next_line_is_split = len(transformed_lines[current_index+1].split('<newline>')) > 1
                     if next_line_is_split:
-                        next_secnd_half = corrected_lines[current_index+1].split('<newline>')[1]
+                        next_secnd_half = transformed_lines[current_index+1].split('<newline>')[1]
                 try:
-                    last_line = corrected_lines[current_index-1]
-                    last_first_half = last_line.split('<newline>')[0].strip()
+                    last_line = transformed_lines[current_index-1]
+                    # last_first_half = last_line.split('<newline>')[0].strip()
                     last_secnd_half = last_line.split('<newline>')[1].strip()
                 except IndexError:
                     pass
-                try:
-                    last_last_line = corrected_lines[current_index-2]
-                    last_last_first_half = last_last_line.split('<newline>')[0].strip()
-                    last_last_secnd_half = last_last_line.split('<newline>')[1].strip()
-                except IndexError:
-                    pass
-                try: 
-                    next_next_line = corrected_lines[current_index+2]
-                    next_next_first_half = next_next_line.split('<newline>')[0].strip()
-                    next_next_secnd_half = next_next_line.split('<newline>')[1].strip()
-                except IndexError:
-                    pass
+                # try:
+                    # last_last_line = transformed_lines[current_index-2]
+                    # last_last_first_half = last_last_line.split('<newline>')[0].strip()
+                    # last_last_secnd_half = last_last_line.split('<newline>')[1].strip()
+                # except IndexError:
+                #    pass
+                # try: 
+                #    next_next_line = transformed_lines[current_index+2]
+                    # next_next_first_half = next_next_line.split('<newline>')[0].strip()
+                    # next_next_secnd_half = next_next_line.split('<newline>')[1].strip()
+                # except IndexError:
+                #    pass
                 # This is only for the fist line. Special case because of the indexing.
                 if current_index == 0:
                     yield curr_first_half
 
                 else:
-                    if all([curr_secnd_half, next_first_half, last_secnd_half]):
-                        first_word_in_current_second_half = curr_secnd_half.split()[0]
-                        first_word_in_next_first_half = next_first_half.split()[0]
-                        last_word_in_last_second_half = last_secnd_half.split()[-1]
-                        if last_word_in_last_second_half.endswith('-'):
-                            last_word_in_last_second_half = last_word_in_last_second_half[:-1]
-                        combined_curr_secnd_last_secnd = last_word_in_last_second_half + first_word_in_current_second_half
-                        combined_next_first_last_secnd = last_word_in_last_second_half + first_word_in_next_first_half
-                        if combined_curr_secnd_last_secnd != combined_next_first_last_secnd:
-                            if exists_in_bin_or_old_words(combined_next_first_last_secnd):
-                                current_line_out = next_first_half
-                            else:
-                                current_line_out = curr_secnd_half
+                    # The following code block has to do with picking a line based on whether the first word in the following
+                    # line and the last word in the current line can be conjoined into a single known word. If not, it picks
+                    # curr_secnd_half
                     if all([curr_secnd_half, next_first_half, next_secnd_half]):
                         last_word_in_curr_secnd_half = curr_secnd_half.split()[-1]
                         last_word_in_next_first_half = next_first_half.split()[-1] 
@@ -188,7 +178,24 @@ def process_lines(original_lines, corrected_lines):
                             if exists_in_bin_or_old_words(combined_next_first_next_secnd):
                                 current_line_out = next_first_half
                             else:
-                                current_line_out = curr_secnd_half                            
+                                current_line_out = curr_secnd_half   
+                    # The following block has to do with picking a line based on whether the last word in the preceding
+                    # line and the first word in the current line can be conjoined into a single known word. If not,
+                    # it picks curr_secnd_half
+                    if all([curr_secnd_half, next_first_half, last_secnd_half]):
+                        first_word_in_current_second_half = curr_secnd_half.split()[0]
+                        first_word_in_next_first_half = next_first_half.split()[0]
+                        last_word_in_last_second_half = last_secnd_half.split()[-1]
+                        if last_word_in_last_second_half.endswith('-'):
+                            last_word_in_last_second_half = last_word_in_last_second_half[:-1]
+                        combined_curr_secnd_last_secnd = last_word_in_last_second_half + first_word_in_current_second_half
+                        combined_next_first_last_secnd = last_word_in_last_second_half + first_word_in_next_first_half
+                        if combined_curr_secnd_last_secnd != combined_next_first_last_secnd:
+                            if exists_in_bin_or_old_words(combined_next_first_last_secnd):
+                                current_line_out = next_first_half
+                            else:
+                                current_line_out = curr_secnd_half
+                                             
                 if current_line_out is not None and not ((current_index + 1) == n_lines and current_line_out == ''):
                     yield format_line_out(current_line_out, ocr_junk)
                 current_index += 1  
